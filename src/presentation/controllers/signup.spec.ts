be11b1e5a6d175b2ff77b23585033ba17081034c
anderson-consultant-it <60013836+anderson-consultant-it@users.noreply.github.com>
2020-06-07 17:01:42 -0400

    refactor: move all error imports to a single index file and removed export default

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,6 @@
 import SignUpController from './signup';
-import MissingParamError from '../errors/missing-param-error';
-import ServerError from '../errors/server-error';
+import { MissingParamError, InvalidParamError, ServerError } from '../errors';
 
-import InvalidParamError from '../errors/invalid-param-error';
 import { IEmailValidator } from '../protocols/email-validator';
 
 interface ISutTypes {
