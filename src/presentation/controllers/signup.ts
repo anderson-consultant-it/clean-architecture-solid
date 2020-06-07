86501ec22b0263a3e9c23bf9b5581dac7f5921ea
anderson-consultant-it <60013836+anderson-consultant-it@users.noreply.github.com>
2020-06-07 16:56:49 -0400

    refactor: move server error to http-helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,8 @@
 import MissingParamError from '../errors/missing-param-error';
 import InvalidParamError from '../errors/invalid-param-error';
-import ServerError from '../errors/server-error';
 
 import { IHttpRequest, IHttpResponse } from '../protocols/http';
-import { badRequest } from '../helpers/http-helper';
+import { badRequest, serverError } from '../helpers/http-helper';
 import { IController } from '../protocols/controller';
 import { IEmailValidator } from '../protocols/email-validator';
 
@@ -28,7 +27,7 @@ export default class SignUpController implements IController {
 
       return { statusCode: 200 };
     } catch (error) {
-      return { statusCode: 500, body: new ServerError() };
+      return serverError();
     }
   }
 }
